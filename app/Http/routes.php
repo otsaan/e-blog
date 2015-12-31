010bb9a203f458ce64d9e400f5b45c36eecbdade
Anwar Ziani <anwaar.ziani@gmail.com>
2015-12-31 11:50:38 +0000

    Updates

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,18 +17,6 @@ Route::get('/profile', function () {
 
 
 
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
 Route::group(['middleware' => ['web']], function () {
     //
 });
