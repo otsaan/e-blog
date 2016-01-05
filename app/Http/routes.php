62230511026da9f5bca93920aa7fcb92aeab7b7e
Anwar Ziani <anwaar.ziani@gmail.com>
2016-01-05 00:37:20 +0000

    Update routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,53 +1,39 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/profile', function () {
     return view('profile');
 });
 
+Route::get('/admin', function () {
+    return view('admin.index');
+});
 
 
 Route::group(['domain' => '{username}.localhost', 'middleware' => ['web']], function () {
 
-    Route::get('/', function ($username) {
-        if ($username == 'admin') {
-            return view('admin.index');
-        }
+    Route::get('/', function () {
         return view('blog');
     });
 
 
-    Route::group(['middleware' => 'auth'], function () {
-
-        Route::get('/dashboard', function ($username) {
-            if ($username != 'admin') {
-                return view('index');
-            }
-        });
-    });
-
-
-    Route::get('/login', function () {
-        return view('login');
-    });
-
-    Route::get('/register', function () {
-        return view('register');
-    });
+    Route::get('/dashboard', ['middleware' => 'auth', function () {
+        return view('index');
+    }]);
 });
 
 
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
-    Route::get('/home', 'HomeController@index');
+});
+
+Route::get('/', function() {
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
 });
