22dbfa8f5cb0bdd118970f9fbeecee6e904a8b49
Saad El Hajjaji <saadelha@gmail.com>
2016-01-11 21:08:59 +0000

    Fix deleting articles

--- public/js/main.js
+++ public/js/main.js
@@ -4,11 +4,12 @@ new Vue({
     data: {
         title: 'Nouvel article',
         submit: 'Valider',
+        annuler: false,
         article: {
             id: 0,
             title: ''
         },
-        category: '',
+        category: 1,
         action: ''
     },
 
@@ -17,6 +18,7 @@ new Vue({
             this.title = 'Modifier article';
             this.submit = 'Modifier';
             this.action = '/articles';
+            this.annuler = true;
 
             $.getJSON('/api/article/' + id, function(article) {
                 this.article = article;
@@ -29,6 +31,40 @@ new Vue({
             e.preventDefault();
             $('#content').val($('#summernote').summernote('code'));
             $('form').unbind('submit').submit();
+        },
+
+        delete: function(id) {
+            swal({title: "Voulez-vous vraiment supprimer cet article?",
+                text: "Cet action est irréversible!",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Oui, supprimer!",
+                closeOnConfirm: false,
+                showLoaderOnConfirm: true,
+            }, function(){
+                $.ajax({
+                    type: 'POST',
+                    url: "/articles/" + id,
+                    data: {
+                        _method: 'DELETE',
+                        _token: $('meta[name="csrf-token"]').attr('content')
+                    },
+                    success: function (data) {
+                        $('#tr'+id).hide();
+                        swal({
+                            title: "",
+                            text: "Article supprimé!",
+                            showConfirmButton: true,
+                            type: "success"
+                        })
+                    },
+                    error: function (err) {
+                        console.log(err);
+                    }
+                });
+            });
         }
     }
 });
+
