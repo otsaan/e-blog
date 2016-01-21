4f753952aaf86057ff249f93959ec5638cde4ce1
Saad El Hajjaji <saadelha@gmail.com>
2016-01-21 01:55:00 +0000

    Add categories in sidebar

--- public/pages/datatables.editable.init.js
+++ public/pages/datatables.editable.init.js
@@ -129,7 +129,6 @@
 				'<a href="#" class="hidden on-editing save-row"><i class="fa fa-save"></i></a>',
 				'<a href="#" class="hidden on-editing cancel-row"><i class="fa fa-times"></i></a>',
 				'<a href="#" class="on-default edit-row"><i class="fa fa-pencil"></i></a>',
-				'<a href="#" class="on-default remove-row"><i class="fa fa-trash-o"></i></a>'
 			].join(' ');
 
 			data = this.datatable.row.add(['', actions ]);
