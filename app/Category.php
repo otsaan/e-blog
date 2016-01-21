0c96f75adea2fd3577b53f2b5b2d32d5c3d8f093
Saad El Hajjaji <saadelha@gmail.com>
2016-01-21 01:49:40 +0000

    Add and update category

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
     public function articles()
     {
         return $this->belongsToMany('App\Article');
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -9,5 +10,28 @@ use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $categories = Category::all();
+
+        return view('admin.categories')->with([
+            'categories' => $categories
+        ]);
+    }
+
+    public function create(Request $request)
+    {
+        return Category::create([
+            'name' => $request['name']
+        ]);
+    }
+
+    public function update($username, $id, Request $request)
+    {
+        $category = Category::find($id);
+        $category->name = $request['name'];
+        $category->save();
+    }
+
 }
